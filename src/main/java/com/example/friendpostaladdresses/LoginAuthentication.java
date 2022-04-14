f8781f89046014e37338026e85c7d7a1feee6848
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-04-14 13:32:22 +0300

    Login toimivuus

---
+++ src/main/java/com/example/friendpostaladdresses/LoginAuthentication.java
@@ -0,0 +1,78 @@
+package com.example.friendpostaladdresses;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class LoginAuthentication extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/login_success")
+                .successForwardUrl("/login_success_handler")
+                .failureForwardUrl("/login_failure_handler")
+                .and()
+                .logout()
+                .logoutUrl("/doLogout")
+                .logoutSuccessUrl("/logout_success")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                            Authentication authentication)
+                            throws IOException, ServletException {
+
+                        System.out.println("This user logged out: " + authentication.getName());
+
+                        response.sendRedirect("/logout_success");
+                    }
+                });
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("USER", "ADMIN")
+                .build();
+
+        users.add(user);
+
+        user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+        users.add(user);
+
+        return new InMemoryUserDetailsManager(users);
+    }
+}
--- src/main/java/com/example/friendpostaladdresses/web/FriendpostaladdressesController.java
+++ src/main/java/com/example/friendpostaladdresses/web/FriendpostaladdressesController.java
@@ -9,21 +9,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.friendpostaladdresses.domain.Friend;
 import com.example.friendpostaladdresses.domain.FriendRepository;
 
+@CrossOrigin
 @Controller
 public class FriendpostaladdressesController {
 	@Autowired
 	private FriendRepository frepository;
 
-//listaa
+	// listaa
 	@RequestMapping(value = { "/", "/friendlist" })
 	public String friendList(Model model) {
 		model.addAttribute("friends", frepository.findAll());
@@ -31,26 +33,30 @@ public class FriendpostaladdressesController {
 		return "Friendlist";
 	}
 
-//Listaa kaikki kirjat REST avulla
-	@RequestMapping(value = "/friends", method = RequestMethod.GET)
-	public @ResponseBody List<Friend> friendListRest() {
-		return (List<Friend>) frepository.findAll();
-	}
-
-//Listaa kirja RESTful ja ID avulla
-	@RequestMapping(value = "/friend/{id}", method = RequestMethod.GET)
-	public @ResponseBody Optional<Friend> findFriendRest(@PathVariable("id") Long friendId) {
-		return frepository.findById(friendId);
-	}
-
-//lisää kirja
+	/*
+	 * // Listaa kaikki kirjat REST avulla
+	 * 
+	 * @RequestMapping(value = "/friends", method = RequestMethod.GET)
+	 * public @ResponseBody List<Friend> friendListRest() {
+	 * return (List<Friend>) frepository.findAll();
+	 * }
+	 * 
+	 * // Listaa kirja RESTful ja ID avulla
+	 * 
+	 * @RequestMapping(value = "/friend/{id}", method = RequestMethod.GET)
+	 * public @ResponseBody Optional<Friend> findFriendRest(@PathVariable("id") Long
+	 * friendId) {
+	 * return frepository.findById(friendId);
+	 * }
+	 */
+	// lisää kirja
 	@RequestMapping(value = "/add")
 	public String addFriend(Model model) {
 		model.addAttribute("friend", new Friend());
 		return "addFriend";
 	}
 
-//tallentaa
+	// tallentaa
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Friend friend) {
 		System.out.println("TEST" + friend);
@@ -58,18 +64,39 @@ public class FriendpostaladdressesController {
 		return "redirect:friendlist";
 	}
 
-//delete
+	// delete
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteFriend(@PathVariable("id") Long friendId, Model model) {
 		frepository.deleteById(friendId);
 		return "redirect:../friendlist";
 	}
 
-//edittaus
+	// edittaus
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editFriend(@PathVariable("id") Long friendId, Model model) {
 		System.out.println("I GOT HERE");
 		model.addAttribute("friend", frepository.findById(friendId));
 		return "editFriend.html";
 	}
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	@PostMapping("/login_success_handler")
+	public String loginSuccessHandler(Model model) {
+		model.addAttribute("friends", frepository.findAll());
+		System.out.println("Logging user login success...");
+
+		return "Friendlist";
+	}
+
+	@PostMapping("/login_failure_handler")
+	public String loginFailureHandler() {
+		System.out.println("Login failure handler....");
+
+		return "login";
+	}
+
 }
